f1161c2f5222063bf82223fe7d6d3381fcd676b1
Fabio Souza Miranda <fabiosouza21777@gmail.com>
2025-05-24 11:16:21 -0300

    Adicionando kafka

--- loja_livros_virtual/boot/src/main/java/com/fsm/livraria/kafka/CompraValidateListener.java
+++ loja_livros_virtual/boot/src/main/java/com/fsm/livraria/kafka/CompraValidateListener.java
@@ -7,11 +7,15 @@ import com.fsm.livraria.dto.compra.CompraDto;
 import com.fsm.livraria.repositories.CompraRepository;
 import io.micronaut.configuration.kafka.annotation.KafkaListener;
 import io.micronaut.configuration.kafka.annotation.Topic;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @KafkaListener
 public class CompraValidateListener {
 
     private final CompraRepository repository;
+
+    private static final Logger logger = LoggerFactory.getLogger(CompraValidateListener.class);
 
     public CompraValidateListener(CompraRepository repository) {
         this.repository = repository;
@@ -20,21 +24,26 @@ public class CompraValidateListener {
     @Topic("validar-compra")
     public void validarComprar(CompraDto compra){
 
-        System.out.println("COMPRA LIDA");
+        try { //caso o corro algum erro no processamento e o erro não for tratado a configuração padrão não vai deixar seguir para a proxima mensage é boa prática sempre tratar os erros das mensagem
+            logger.info("COMPRA LIDA");
 
-        System.out.println(compra.toString());
+            logger.info(compra.toString());
 
-        /**
-         * logica de validação a partir daqui
-         * */
+            //logica de validação
+            Compra compraEntity = repository.findByUuidLazy(compra.getUuid()).orElseThrow(() -> new NotFoundError("compra não encontrada"));
 
-        Compra compraEntity = repository.findByUuidLazy(compra.getUuid()).orElseThrow(() -> new NotFoundError("compra não encontrada"));
+            compraEntity.setStatus(CompraStatus.FINALIZADA);
 
-        compraEntity.setStatus(CompraStatus.FINALIZADA);
+            repository.update(compraEntity);
 
-        repository.update(compraEntity);
+            logger.info("======================processamento kafka finalizado com sucesso :)=================================================");
 
-        System.out.println("======================processamento kafka finalizado com sucesso :)=================================================");
+        }catch (Exception e){
+            logger.error("erro - {}", e.getMessage(), e);
+
+            //aqui o ideal e envia para uma fila morta o conceito de dead letter queue, para processar depois
+
+        }
 
     }
 
